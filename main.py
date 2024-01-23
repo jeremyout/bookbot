aaea6da566ce783c7a7c4b78335afe4a3c71ac6f
jeremyout <22989572+jeremyout@users.noreply.github.com>
2024-01-22 22:56:18 -0600

    Add function to print out a report

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    # print(text)
-    print(get_number_of_words(text))
-    print(count_character_dict(text))
-
+    number_of_words = get_number_of_words(text)
+    char_dict = count_character_dict(text)
+    generate_report(book_path, number_of_words, char_dict)
 
 def get_book_text(path):
     with open(path) as f:
@@ -22,6 +21,18 @@ def count_character_dict(text):
         else:
             letter_dict[char] = 1
     return letter_dict
+
+def generate_report(path, word_count, character_dict):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document")
+    list_of_chars = list(character_dict.items())
+    list_of_chars.sort(key=lambda x: x[1], reverse=True)
+    
+    for char in list_of_chars:
+        if char[0].isalpha():
+            print(f"The {char[0]} character was found {char[1]} times")
+
+    print("--- End report ---")
 
 
 main()
