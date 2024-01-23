5a8c28b36b13ce56ef3e795c33118eac32431d1e
jeremyout <22989572+jeremyout@users.noreply.github.com>
2024-01-22 22:26:39 -0600

    Add function to get a dictionary with all characters in a book

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = get_book_text(book_path)
     # print(text)
     print(get_number_of_words(text))
+    print(count_character_dict(text))
 
 
 def get_book_text(path):
@@ -12,6 +13,15 @@ def get_book_text(path):
 def get_number_of_words(text):
     words = text.split()
     return len(words)
+
+def count_character_dict(text):
+    letter_dict = {}
+    for char in text.lower():
+        if char in letter_dict:
+            letter_dict[char] += 1
+        else:
+            letter_dict[char] = 1
+    return letter_dict
 
 
 main()
